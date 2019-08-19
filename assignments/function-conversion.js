f173174c0e54a02d720f088b38d6ceadbef2d869
Francis Bulus <notchera@gmail.com>
2019-08-19 13:30:21 +0100

    updated chellenge 5 solution in arrays.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -31,12 +31,13 @@ let subtract = (param1, param2) => {
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-// console.log(triple);
+exampleArray = [1,2,3,4];
 const triple = exampleArray.map((num) => {
 	return num * 3;
 });
+console.log(triple);
 
